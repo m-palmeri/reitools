8884d5ac37026526b30855163cb52ca8cb6ab0d5
Matthew Palmeri <matthew.thomas.palmeri@gmail.com>
2025-05-07 17:23:45 -0500

    adding lots more required parameters for buy-and-hold analysis tool

--- R/analysis.R
+++ R/analysis.R
@@ -33,14 +33,31 @@ Analysis <- R6::R6Class(
   private = list(
     .fixed_costs = list(),
     .variable_costs = list(),
+    simulation_N = 100000,
+    add_fixed_cost = function(value, name) {
+      check_name(name, arg = name)
+      check_number_decimal(value, arg = name)
+      self$fixed_costs[[name]] <- value
+    },
     add_variable_cost = function(distribution, name) {
       check_name(name, arg = name)
       check_distribution(distribution, arg = name)
       self$variable_costs[[name]] <- distribution
+    },
+    .evaluate_scenario = function() {
+      stop("build out in subclass")
     }
   ),
   public = list(
+    run_simulation = function(N = self$simulation_N) {
+      sim_results <- purrr::map(seq_len(N), function(i) {
+        private$evaluate_scenario()
+      })
 
+      self$sim_results <- sim_results
+
+      invisible(self)
+    }
   )
 )
 
@@ -71,26 +88,102 @@ AnalysisBH <- R6::R6Class(
         check_number_decimal(value, arg = "mortgage_payment")
         self$fixed_costs$mortgage_payment <- value
       }
-    }
+    },
+    rent = function(value) {
+      if (missing(value)) {
+        return(self$variable_costs$rent)
+      } else {
+        check_distribution(value, arg = "rent")
+        self$variable_costs$rent <- value
+      }
+    },
+    property_taxes = function(value) {
+      if (missing(value)) {
+        return(self$variable_costs$property_taxes)
+      } else {
+        check_distribution(value, arg = "property_taxes")
+        self$variable_costs$property_taxes <- value
+      }
+    },
+    insurance = function(value) {
+      if (missing(value)) {
+        return(self$variable_costs$insurance)
+      } else {
+        check_distribution(value, arg = "insurance")
+        self$variable_costs$insurance <- value
+      }
+    },
+    maintenance = function(value) {
+      if (missing(value)) {
+        return(self$variable_costs$maintenance)
+      } else {
+        check_distribution(value, arg = "maintenance")
+        self$variable_costs$maintenance <- value
+      }
+    },
+    vacancy = function(value) {
+      if (missing(value)) {
+        return(self$variable_costs$vacancy)
+      } else {
+        check_distribution(value, arg = "vacancy")
+        self$variable_costs$vacancy <- value
+      }
+    },
+    capital_expenditures = function(value) {
+      if (missing(value)) {
+        return(self$variable_costs$capital_expenditures)
+      } else {
+        check_distribution(value, arg = "capital_expenditures")
+        self$variable_costs$capital_expenditures <- value
+      }
+    },
+    property_management = function(value) {
+      if (missing(value)) {
+        return(self$variable_costs$property_management)
+      } else {
+        check_distribution(value, arg = "property_management")
+        self$variable_costs$property_management <- value
+      }
+    },
   ),
   private = list(
-    simulation_N = numeric()
+    evaluate_scenario = function() {
+
+    }
   ),
   public = list(
     initialize = function(purchase_price,
                           down_payment,
                           mortgage_payment,
-                          ...,
+                          rent,
+                          property_taxes,
+                          insurance,
+                          maintenance,
+                          vacancy,
+                          capital_expenditures,
+                          property_management,
+                          fixed_monthly_extras = NULL,
+                          variable_extras = NULL,
                           simulation_N = 100000) {
       self$purchase_price <- purchase_price
       self$down_payment <- down_payment
       self$mortgage_payment <- mortgage_payment
+      self$rent <- rent
+      self$property_taxes <- property_taxes
+      self$insurance <- insurance
+      self$maintenance <- maintenance
+      self$vacancy <- vacancy
+      self$capital_expenditure <- capital_expenditure
+      self$property_management <- property_management
 
       check_number_whole(simulation_N)
       private$simulation_N <- simulation_N
 
-      dots <- list(...)
-      purrr::iwalk(dots, private$add_variable_cost)
+      check_list(fixed_extras, allow_null = TRUE)
+      purrr::iwalk(fixed_extras, private$add_fixed_cost)
+
+      check_list(variable_extras, allow_null = TRUE)
+      purrr::iwalk(variable_extras, private$add_variable_cost)
     }
   )
 )
