16950f2e74a32c673752d142f704143708a25d94
Matthew Palmeri <matthew.thomas.palmeri@gmail.com>
2025-05-14 18:39:13 -0500

    adding toStringWithAnd helper util function for natural language expressions in print statement and error messages

---
+++ R/utils.R
@@ -0,0 +1,16 @@
+toStringWithAnd <- function(x, quote = FALSE) {
+  x <- as.character(x)
+  if (quote) {
+    x <- sQuote(x)
+  }
+  n <- length(x)
+  if (n == 0) {
+    return("")
+  } else if (n == 1) {
+    return(x)
+  } else if (n == 2) {
+    return(paste(x, collapse = " and "))
+  } else {
+    return(paste(paste(x[1:(n-1)], collapse = ", "), "and", x[n]))
+  }
+}
